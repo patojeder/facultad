5993f122dcc46b6cdb13a857c154a508e7950fb2
Velaztiqui Trigo Mauro Nicolas <trigotex@gmail.com>
2024-11-01 19:47:32 -0300

    Avanzando2

--- obj/Debug/net8.0/tl2-tp6-2024-Trigo00.AssemblyInfo.cs
+++ obj/Debug/net8.0/tl2-tp6-2024-Trigo00.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tl2-tp6-2024-Trigo00")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4cc12745cd865a6cecb27a373d590059dd9e7bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b70992bec219dec4296c766f48f8d5c44ae3e7d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("tl2-tp6-2024-Trigo00")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tl2-tp6-2024-Trigo00")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
