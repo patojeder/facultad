dedaaf66797fc538b5b32d3703862d0a72b9fb11
Trigo00 <trigotex@gmail.com>
2024-11-08 23:24:13 -0300

    8/11 23.24

--- obj/Debug/net8.0/tl2-tp6-2024-Trigo00.AssemblyInfo.cs
+++ obj/Debug/net8.0/tl2-tp6-2024-Trigo00.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tl2-tp6-2024-Trigo00")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7146dbb5b343b40170528687b7d9b0a07634561")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7db50d4eec72d6d081758b7c27bc342859f599c")]
 [assembly: System.Reflection.AssemblyProductAttribute("tl2-tp6-2024-Trigo00")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tl2-tp6-2024-Trigo00")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
